305f789b1c2314a7ea11f52d5d7df5b6e632d554
chengguizi <cgz_05@sina.com>
2014-08-20 14:27:27 +0800

    Disable OPTFLOW

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -37,7 +37,7 @@
 
 // features below are disabled by default on all boards
 // CHM - this is the code that enable OPTFLOW globally!!!
-#define OPTFLOW               ENABLED             // enable optical flow sensor and OF_LOITER flight mode at a cost of 5K of flash space
+//#define OPTFLOW               ENABLED             // enable optical flow sensor and OF_LOITER flight mode at a cost of 5K of flash space
 //#define SPRAYER               ENABLED             // enable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
 //#define EPM_ENABLED           ENABLED             // enable epm cargo gripper costs 500bytes of flash
 #define NAV_GUIDED            ENABLED             // enable external navigation computer to control vehicle through MAV_CMD_NAV_GUIDED mission commands
